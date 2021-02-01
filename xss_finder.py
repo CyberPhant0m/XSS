9e9d5854bf3a246794f4e56f34b5a6f319e85201
Cyber Phantom <67561692+CyberPhant0m@users.noreply.github.com>
2021-01-31 20:41:36 -0500

    Add files via upload

--- xss_finder.py
+++ xss_finder.py
@@ -1,15 +1,16 @@
 import requests
 from colorama import (init, Fore, Back,)
 import argparse
+import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-u", help="Especificar URL")
-parser.add_argument("-p", help="Especificar posible parametro vulnerable")
-parser.add_argument("-f", help="Especificar archivo con payloads ")
+parser.add_argument("-u", "--url", help="Especificar URL")
+parser.add_argument("-p", "--parametro", help="Especificar posible parametro vulnerable")
+parser.add_argument("-f", "--file", help="Especificar archivo con payloads ")
 args = parser.parse_args()
-url = args.u
-uri = args.p
-file2 = args.f
+url = args.url
+uri = args.parametro
+file2 = args.file
 
 init()
 good = Fore.GREEN+""+Back.BLACK+"[+]"+Fore.RESET+Back.RESET
@@ -18,41 +19,48 @@ fondo = Back.LIGHTRED_EX
 fondo1 = Back.LIGHTBLUE_EX
 nofondo = Back.RESET
 
-if file2 ==False or url ==False:
-    print(f"{good} {fondo}Intentando otro metodo de desbloqueo {nofondo}")
-else:
-    archivo = open(file2,"r")
-
 def start():
-    try:
-        x = 0
+    if file2 ==None or url ==None or uri ==None:
+        print(f"{good} {fondo}Llena los parametros. {nofondo}")
+        parser.print_help()
+    else:
         print(f"{good} {fondo1}{Fore.WHITE}Iniciando{nofondo}")
-        for line in archivo:
-            payload = {f"{uri}":f"{line}"}
-            request = requests.get(url,params=payload)
-            if request.status_code == 200 and line in requests.get(url,params=payload).text:
-                print(f"{good} {fondo1} {Fore.WHITE}200 XSS FOUND: {request.url} {nofondo}")
-            if request.status_code == 302:
-                print(f"{bad} {fondo1}{Fore.WHITE}302 Found {request.url} {nofondo}")
-            if request.status_code == 304:
-                print(f"{bad} {fondo1}{Fore.WHITE}304 Not Modified {request.url} {nofondo}")    
-            if request.status_code == 403:
-                print(f"{bad} {fondo1}{Fore.WHITE}403 Forbidden {request.url} {nofondo}")   
-            if request.status_code == 404:
-                print(f"{bad} {fondo1}{Fore.WHITE}404 Not Found {request.url} {nofondo}")
-            if request.status_code == 409:
-                print(f"{bad} {fondo1}{Fore.WHITE}409 Conflict {request.url} {nofondo}")
-            if request.status_code == 500:
-                print(f"{bad} {fondo1}{Fore.WHITE}500 Internal Server Error {request.url} {nofondo}")
+        try:
+            x = 0
+            if os.path.exists(file2):
+                archivo = open(file2,"r")
             else:
-                print(f"{bad} {fondo1}{Fore.WHITE}Al parecer el parametro no existe u ocurrió otro error {nofondo}")
-            x = x + 1
-    except KeyboardInterrupt:
-        print(f"{good} {fondo}Proceso interrumpido {nofondo}")
-    except requests.exceptions.MissingSchema:
-        print(f"{bad} {fondo}URL invalida {nofondo}")
-    except requests.exceptions.ConnectionError:
-        print(f"{bad} {fondo}No se pudo conectar al host.{nofondo}")
+                print(f"{bad} {fondo1}{Fore.WHITE}El archivo no existe. {nofondo}")
+            for line in archivo:
+                payload = {f"{uri}":f"{line}"}
+                request = requests.get(url,params=payload)
+                if request.status_code == 200 and line in requests.get(url,params=payload).text:
+                    print(f"{good} {fondo1} {Fore.WHITE}200 XSS FOUND: {request.url} {nofondo}")
+                elif request.status_code == 302:
+                    print(f"{bad} {fondo1}{Fore.WHITE}302 Found {request.url} {nofondo}")
+                elif request.status_code == 304:
+                    print(f"{bad} {fondo1}{Fore.WHITE}304 Not Modified {request.url} {nofondo}")    
+                elif request.status_code == 403:
+                    print(f"{bad} {fondo1}{Fore.WHITE}403 Forbidden {request.url} {nofondo}")   
+                elif request.status_code == 404:
+                    print(f"{bad} {fondo1}{Fore.WHITE}404 Not Found {request.url} {nofondo}")
+                elif request.status_code == 409:
+                    print(f"{bad} {fondo1}{Fore.WHITE}409 Conflict {request.url} {nofondo}")
+                elif request.status_code == 500:
+                    print(f"{bad} {fondo1}{Fore.WHITE}500 Internal Server Error {request.url} {nofondo}")
+                else:
+                    print(f"{bad} {fondo1}{Fore.WHITE}Al parecer el parametro no existe u ocurrió otro error {nofondo}")
+                x = x + 1
+        except KeyboardInterrupt:
+            print(f"{good} {fondo}Proceso interrumpido {nofondo}")
+        except requests.exceptions.MissingSchema:
+            print(f"{bad} {fondo}URL invalida {nofondo}")
+        except requests.exceptions.ConnectionError:
+            print(f"{bad} {fondo}No se pudo conectar al host.{nofondo}")
+        except FileNotFoundError:
+            print(f"{bad} {fondo1}{Fore.WHITE}El archivo no existe. {nofondo}")
+        except UnboundLocalError:
+            parser.print_help()
 
 def credit():
     logo = Fore.RED + """
